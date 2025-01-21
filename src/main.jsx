144bf3589c255ac4522f54382520ebf9142927c8
John Siordia <siohn@proton.me>
2025-01-21 15:07:59 -0600

    basename change

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./components/App/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <HashRouter basename="/ahaworks">
       <App />
     </HashRouter>
   </StrictMode>
