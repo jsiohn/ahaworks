3aa22e7b312b4f32b20a6a2840cae46b5be83be0
John Siordia <siohn@proton.me>
2025-01-21 15:21:23 -0600

    changes test

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./components/App/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter basename="/ahaworks/">
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>
